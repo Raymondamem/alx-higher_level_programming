d39df28cd687a40ab90834b9477be0ec7fc77064
root <raymondamem525@gmail.com>
2023-08-14 15:21:20 +0000

    done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    _add = add(a, b)
+    _sub = sub(a, b)
+    _mul = mul(a, b)
+    _div = div(a, b)
+    print("{} + {} = {}".format(a, b, _add))
+    print("{} - {} = {}".format(a, b, _sub))
+    print("{} * {} = {}".format(a, b, _mul))
+    print("{} / {} = {}".format(a, b, _div))
