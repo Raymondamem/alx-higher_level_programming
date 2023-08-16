d05c1b4d0c013ceac0aa1535e402642c3ee1d450
root <raymondamem525@gmail.com>
2023-08-16 16:06:09 +0000

    done

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
+
 def delete_at(my_list=[], idx=0):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
+
+    if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    
-    # Create a new list with elements before idx
-    new_list = my_list[:idx]
-    
-    # Extend the new list with elements after idx (skipping the idx element)
-    new_list.extend(my_list[idx + 1:])
-    
-    return new_list
+
+    else:
+        del my_list[idx]
+
+    return my_list
