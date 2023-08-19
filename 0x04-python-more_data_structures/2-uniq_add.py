bd9a330b73851b44f75136c70739db90c52f2f24
root <raymondamem525@gmail.com>
2023-08-19 08:50:43 +0000

    done

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    my_new_list = []
+    my_sum = 0
+    for my_num in my_list:
+        if my_num not in my_new_list:
+            my_sum += my_num
+            my_new_list.append(my_num)
+    return my_sum
