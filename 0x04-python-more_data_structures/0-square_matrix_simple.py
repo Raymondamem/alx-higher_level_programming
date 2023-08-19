1840e7a9bd57e41be0cf7a0007a8340398db4cf1
root <raymondamem525@gmail.com>
2023-08-19 08:29:37 +0000

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(_matrix=[]):
+def square_matrix_simple(matrix=[]):
     n_matrix = []
-    for _col in _matrix:
+    for _col in matrix:
         _rst = list(map(lambda x: x**2, _col))
         n_matrix.append(rst)
     return n_matrix
