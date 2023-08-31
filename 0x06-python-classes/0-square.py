5054595cac1d50a8f7c8f813ef7933c8432de6ce
root <raymondamem525@gmail.com>
2023-08-31 05:06:32 +0000

    done

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
 class Square:
+    """This class is a square.
+    Attribute:
+        None
+    Method:
+        None
+    """
     pass
