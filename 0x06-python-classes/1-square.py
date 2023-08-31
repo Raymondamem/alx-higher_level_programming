c88f300e5fbfd95c4a5b712711344d7631ec051a
root <raymondamem525@gmail.com>
2023-08-31 05:12:05 +0000

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """A class Square that defines a square by its size as private"""
+    def __init__(self, size):
+        """Initialize the square with the given size as private"""
+        self.__size = size
