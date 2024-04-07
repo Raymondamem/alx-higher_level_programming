ba705989903ab808ce43a06f6c823bd44aa6dda0
Raymondamem <raymondamem525@gmail.com>
2024-04-07 14:35:27 +0000

    function def roman_to_int(roman_string): that converts a Roman numeral to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def to_subtract(list_num):
     to_sub = 0
     max_list = max(list_num)
@@ -13,13 +12,11 @@ def to_subtract(list_num):
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-
     if not isinstance(roman_string, str):
         return 0
 
     rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
-
     num = 0
     last_rom = 0
     list_num = [0]
